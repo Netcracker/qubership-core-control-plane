962cd05ab707477e09c2d0fc171ac6a7a1fb7355
Ksiona <kseniia.shmoilova.qubership@gmail.com>
2025-07-14 18:49:52 +0400

    test

--- control-plane/it_http_filter_test.go
+++ control-plane/it_http_filter_test.go
@@ -18,27 +18,6 @@ func Test_IT_ResponseTest_ResponseContainsUUIDHeader(t *testing.T) {
 	defer traceSrvContainer1.Purge()
 
 	prefix := "/api/v1/test-header/a1b2c3d4-e5f6-7890-1234-567890abcdef"
-	
-/* 	routeConfig := `apiVersion: nc.core.mesh/v3
-kind: RouteConfiguration
-metadata:
- name: filter-routes
- namespace: ''
-spec:
- gateways:
-   - internal-gateway-service
- virtualServices:
- - name: internal-gateway-service
-   routeConfiguration:
-     routes:
-     - destination:
-         cluster: filter-routes-test
-         endpoint: filter-routes-test:8080
-       rules:
-       - match:
-           prefix: /api/v1/test-header/a1b2c3d4-e5f6-7890-1234-567890abcdef
-		   luaFilter: my-lua-filter` */
-
 
 	filterConfig := `apiVersion: nc.core.mesh/v3
 kind: HttpFilters
@@ -46,12 +25,18 @@ spec:
   gateways:
     - internal-gateway-service
   luaFilters:
-    - name: my-lua-filter
-      luaScript: "function envoy_on_request(request_handle)\n  local path = request_handle:headers():get(\":path\")\n  local match = string.match(path, \"([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})\")\n    if match then\n        request_handle:headers():add(\"x-uuid\", match)\n  end\nend"
+    - name: test-lua-filter
+      luaScript: "
+		function envoy_on_request(request_handle)\n
+			local path = request_handle:headers():get(\":path\")\n
+			local match = string.match(path, \"([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})\")\n
+			if match then\n
+				request_handle:headers():add(\"x-uuid\", match)\n
+			end\n
+		end"
 `
 	
 	internalGateway.ApplyConfigAndWait(assert, 60*time.Second, filterConfig)
-	//internalGateway.ApplyConfigAndWait(assert, 60*time.Second, routeConfig)
 	internalGateway.RegisterRoutesAndWait(
 		assert,
 		60*time.Second,
@@ -59,7 +44,7 @@ spec:
 		dto.RouteV3{
 			Destination: dto.RouteDestination{Cluster: TestCluster, Endpoint: TestEndpointV1},
 			Rules: []dto.Rule{
-				{Match: dto.RouteMatch{Prefix: prefix}, LuaFilter: "my-lua-filter"},
+				{Match: dto.RouteMatch{Prefix: prefix}, LuaFilter: "test-lua-filter"},
 			},
 		},
 	)
@@ -68,9 +53,16 @@ spec:
 	envoyConfigDump := internalGateway.GetEnvoyConfigJson(assert)
     log.Info("Internal-gateway config dump: \n %v", envoyConfigDump)
 
- 	headers := make(http.Header)
-	testResponseHeaders(assert, internalGateway.Url+prefix, headers, http.StatusOK,
-		map[string]string{"x-uuid": "a1b2c3d4-e5f6-7890-1234-567890abcdef"})
+	resp, statusCode := GetFromTraceService(assert, internalGateway.Url+prefix)
+		assert.Equal(http.StatusOK, statusCode)
+		if resp == nil {
+			log.InfoC(ctx, "Didn't receive TraceResponse; status code: %d", statusCode)
+		} else {
+			log.InfoC(ctx, "Trace service response: %v", resp)
+			assert.Equal(prefix, resp.Path)
+			// verify request header x-uuid 
+			assert.Equal("a1b2c3d4-e5f6-7890-1234-567890abcdef", resp.Headers.Get("x-uuid"))
+		}
 
 	// cleanup routes
 	internalGateway.DeleteRoutesAndWait(assert, 60*time.Second, dto.RouteDeleteRequestV3{
@@ -81,5 +73,4 @@ spec:
 			Version: "v1",
 		},
 	}) 
-	//assert.False(checkIfTestRouteWithPrefixIsPresent(assert, cluster, prefix))
 }
