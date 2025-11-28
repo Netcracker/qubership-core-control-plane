8f2c5031071e09e6baadabd1658aa93c8d7a7efb
Evgenii Stetesenko <evgenii.stetsenko.qubership@gmail.com>
2025-11-28 14:50:58 +0500

    feat: generate gateway api httproute CR by Spring annotations

--- httproute-generator/src/main/java/org/qubership/routes/gateway/plugin/HttpRouteGenerator.java
+++ httproute-generator/src/main/java/org/qubership/routes/gateway/plugin/HttpRouteGenerator.java
@@ -14,93 +14,54 @@ import java.util.stream.Collectors;
 public class HttpRouteGenerator {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    public record HTTPRouteResource(
-            String apiVersion,
-            String kind,
-            Metadata metadata,
-            Spec spec
-    ) {
+    public record HTTPRouteResource(String apiVersion, String kind, Metadata metadata, Spec spec) {
         public record Metadata(String name) {
         }
 
-        public record Spec(
-                Set<ParentRef> parentRefs,
-                List<Rule> rules
-        ) {
+        public record Spec(Set<ParentRef> parentRefs, List<Rule> rules) {
             public record ParentRef(String name) {
             }
 
-            public record Rule(
-                    List<Match> matches,
-                    List<BackendRef> backendRefs
-            ) {
+            public record Rule(List<Match> matches, List<BackendRef> backendRefs) {
             }
 
-            public record Match(
-                    Path path
-            ) {
+            public record Match(Path path) {
                 public record Path(String type, String value) {
                 }
             }
 
-            public record BackendRef(
-                    String name,
-                    Integer port
-            ) {
+            public record BackendRef(String name, Integer port) {
             }
         }
     }
 
+    private static List<HTTPRouteResource> createHttpRoutes(String serviceName, int servicePort, Set<HttpRoute> httpRoutes) {
+        Map<HttpRoute.Type, List<HttpRoute>> routesByType = httpRoutes.stream().collect(Collectors.groupingBy(HttpRoute::type));
 
-    private static List<HTTPRouteResource> createHttpRoutes(
-            String serviceName,
-            int servicePort,
-            Set<HttpRoute> httpRoutes
-    ) {
-        Map<HttpRoute.Type, List<HttpRoute>> routesByType =
-                httpRoutes.stream().collect(Collectors.groupingBy(HttpRoute::type));
+        return routesByType.entrySet().stream().map(entry -> {
+            HttpRoute.Type type = entry.getKey();
+            List<HttpRoute> routes = entry.getValue();
 
-        return routesByType.entrySet().stream()
-                .map(entry -> {
-                    HttpRoute.Type type = entry.getKey();
-                    List<HttpRoute> routes = entry.getValue();
+            HTTPRouteResource.Metadata metadata = new HTTPRouteResource.Metadata(serviceName + "-" + type.name().toLowerCase());
 
-                    HTTPRouteResource.Metadata metadata =
-                            new HTTPRouteResource.Metadata(serviceName + "-" + type.name().toLowerCase());
+            HTTPRouteResource.Spec.ParentRef parentRef = new HTTPRouteResource.Spec.ParentRef(type.gatewayName());
 
-                    HTTPRouteResource.Spec.ParentRef parentRef =
-                            new HTTPRouteResource.Spec.ParentRef(type.gatewayName());
+            List<HTTPRouteResource.Spec.Rule> ruleList = new ArrayList<>();
+            List<HTTPRouteResource.Spec.Match> matches = new ArrayList<>();
 
-                    List<HTTPRouteResource.Spec.Rule> ruleList = new ArrayList<>();
-                    List<HTTPRouteResource.Spec.Match> matches = new ArrayList<>();
+            for (HttpRoute route : routes) {
+                var path = convertSpringPathToHttpRoutePath(route.path());
+                matches.add(new HTTPRouteResource.Spec.Match(path));
+            }
 
-                    for (HttpRoute route : routes) {
-                        var path = convertSpringPathToHttpRoutePath(route.path());
-                        matches.add(new HTTPRouteResource.Spec.Match(path));
-                    }
+            List<HTTPRouteResource.Spec.BackendRef> backendRefs = List.of(new HTTPRouteResource.Spec.BackendRef(serviceName, servicePort));
 
-                    List<HTTPRouteResource.Spec.BackendRef> backendRefs = List.of(
-                            new HTTPRouteResource.Spec.BackendRef(
-                                    serviceName,
-                                    servicePort
-                            )
-                    );
+            ruleList.add(new HTTPRouteResource.Spec.Rule(matches, backendRefs));
 
-                    ruleList.add(new HTTPRouteResource.Spec.Rule(matches, backendRefs));
+            HTTPRouteResource.Spec spec = new HTTPRouteResource.Spec(Set.of(parentRef), ruleList);
 
-                    HTTPRouteResource.Spec spec = new HTTPRouteResource.Spec(
-                            Set.of(parentRef),
-                            ruleList
-                    );
-
-                    return new HTTPRouteResource(
-                            "gateway.networking.k8s.io/v1",
-                            "HTTPRoute",
-                            metadata,
-                            spec
-                    );
-                })
-                .toList();
+            return new HTTPRouteResource("gateway.networking.k8s.io/v1", "HTTPRoute", metadata, spec);
+        }).toList();
     }
 
     public static String generateHttpRoutesYaml(String serviceName, int servicePort, Set<HttpRoute> httpRoutes) {
@@ -108,15 +69,13 @@ public class HttpRouteGenerator {
 
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        return routes.stream()
-                .map(route -> {
-                    try {
-                        return mapper.writeValueAsString(route);
-                    } catch (JsonProcessingException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .collect(Collectors.joining());
+        return routes.stream().map(route -> {
+            try {
+                return mapper.writeValueAsString(route);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }).collect(Collectors.joining());
     }
 
     private static HTTPRouteResource.Spec.Match.Path convertSpringPathToHttpRoutePath(String springPath) {
