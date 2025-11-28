5957b8295c5aa5494c7717b9a0994872cf8423c5
Evgenii Stetesenko <evgenii.stetsenko.qubership@gmail.com>
2025-11-28 14:49:42 +0500

    feat: generate gateway api httproute CR by Spring annotations

--- httproute-generator/src/main/java/org/qubership/routes/gateway/plugin/HttpRouteGenerator.java
+++ httproute-generator/src/main/java/org/qubership/routes/gateway/plugin/HttpRouteGenerator.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class HttpRouteGenerator {
@@ -27,74 +30,77 @@ public class HttpRouteGenerator {
             public record ParentRef(String name) {
             }
 
-            public record Rule(List<Match> matches) {
+            public record Rule(
+                    List<Match> matches,
+                    List<BackendRef> backendRefs
+            ) {
             }
 
-            public record Match(Path path, List<ForwardTo> forwardTo) {
+            public record Match(
+                    Path path
+            ) {
                 public record Path(String type, String value) {
                 }
+            }
 
-                public record ForwardTo(TargetRef targetRef) {
-                    public record TargetRef(String kind, String name, int port) {
-                    }
-                }
+            public record BackendRef(
+                    String name,
+                    Integer port
+            ) {
             }
         }
     }
 
-    private static List<HTTPRouteResource> createHttpRoutes(String serviceName, int servicePort, Set<HttpRoute> httpRoutes) {
-        Map<HttpRoute.Type, List<HttpRoute>> routesByType = httpRoutes.stream()
-                .collect(Collectors.groupingBy(HttpRoute::type));
 
-        return routesByType.entrySet().stream().map(entry -> {
-            HttpRoute.Type type = entry.getKey();
-            List<HttpRoute> routes = entry.getValue();
+    private static List<HTTPRouteResource> createHttpRoutes(
+            String serviceName,
+            int servicePort,
+            Set<HttpRoute> httpRoutes
+    ) {
+        Map<HttpRoute.Type, List<HttpRoute>> routesByType =
+                httpRoutes.stream().collect(Collectors.groupingBy(HttpRoute::type));
 
-            HTTPRouteResource.Metadata metadata =
-                    new HTTPRouteResource.Metadata(serviceName + "-" + type.name().toLowerCase());
+        return routesByType.entrySet().stream()
+                .map(entry -> {
+                    HttpRoute.Type type = entry.getKey();
+                    List<HttpRoute> routes = entry.getValue();
 
-            HTTPRouteResource.Spec.ParentRef parentRef =
-                    new HTTPRouteResource.Spec.ParentRef(type.gatewayName());
+                    HTTPRouteResource.Metadata metadata =
+                            new HTTPRouteResource.Metadata(serviceName + "-" + type.name().toLowerCase());
 
-            List<HTTPRouteResource.Spec.Rule> ruleList = new ArrayList<>();
-            List<HTTPRouteResource.Spec.Match> matchList = new ArrayList<>();
+                    HTTPRouteResource.Spec.ParentRef parentRef =
+                            new HTTPRouteResource.Spec.ParentRef(type.gatewayName());
 
-            for (HttpRoute httpRoute : routes) {
-                HTTPRouteResource.Spec.Match.Path path =
-                        convertSpringPathToHttpRoutePath(httpRoute.path());
+                    List<HTTPRouteResource.Spec.Rule> ruleList = new ArrayList<>();
+                    List<HTTPRouteResource.Spec.Match> matches = new ArrayList<>();
 
-                HTTPRouteResource.Spec.Match.ForwardTo.TargetRef targetRef =
-                        new HTTPRouteResource.Spec.Match.ForwardTo.TargetRef(
-                                "Service",
-                                serviceName,
-                                servicePort
-                        );
+                    for (HttpRoute route : routes) {
+                        var path = convertSpringPathToHttpRoutePath(route.path());
+                        matches.add(new HTTPRouteResource.Spec.Match(path));
+                    }
 
-                HTTPRouteResource.Spec.Match.ForwardTo forwardTo =
-                        new HTTPRouteResource.Spec.Match.ForwardTo(targetRef);
+                    List<HTTPRouteResource.Spec.BackendRef> backendRefs = List.of(
+                            new HTTPRouteResource.Spec.BackendRef(
+                                    serviceName,
+                                    servicePort
+                            )
+                    );
 
-                HTTPRouteResource.Spec.Match match =
-                        new HTTPRouteResource.Spec.Match(path, List.of(forwardTo));
+                    ruleList.add(new HTTPRouteResource.Spec.Rule(matches, backendRefs));
 
-                matchList.add(match);
-            }
-
-            ruleList.add(new HTTPRouteResource.Spec.Rule(matchList));
-
-            HTTPRouteResource.Spec spec =
-                    new HTTPRouteResource.Spec(
-                            new HashSet<>(Set.of(parentRef)),
+                    HTTPRouteResource.Spec spec = new HTTPRouteResource.Spec(
+                            Set.of(parentRef),
                             ruleList
                     );
 
-            return new HTTPRouteResource(
-                    "gateway.networking.k8s.io/v1",
-                    "HTTPRoute",
-                    metadata,
-                    spec
-            );
-
-        }).toList();
+                    return new HTTPRouteResource(
+                            "gateway.networking.k8s.io/v1",
+                            "HTTPRoute",
+                            metadata,
+                            spec
+                    );
+                })
+                .toList();
     }
 
     public static String generateHttpRoutesYaml(String serviceName, int servicePort, Set<HttpRoute> httpRoutes) {
